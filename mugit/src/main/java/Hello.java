4afea4c6827dfdf1e08f8d24b8fe6267574a0476
mujingy <945044871@qq.com>
2019-03-07 20:27:30 +0800

    first commit

---
+++ mugit/src/main/java/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * Hello
+ *
+ * @author 穆井阳 (Employee ID: 88401478)
+ * @version 1.0.0,
+ * @since 1.0.0, 2019/3/7 20:05
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world!!!");
+    }
+}
